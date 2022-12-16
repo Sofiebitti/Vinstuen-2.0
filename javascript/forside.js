df31ec3f779bafad4b47df95381859c5815808f7
Sofie <eaaspeder@students.eaaa.dk>
2022-12-16 14:50:27 +0100

    hej hej sorry

--- javascript/forside.js
+++ javascript/forside.js
@@ -1,7 +1,12 @@
 const slideshow = [
-  "images/billede 2.jpg",
-  "images/vinstue-idag.jpg",
-  "images/billede 5.jpg",
+  "img/v2.jpg",
+  "img/v1.jpg",
+  "img/v3.jpg",
+  "img/v4.jpg",
+  "img/v5.jpg",
+  "img/v6.jpg",
+  "img/v7.jpg",
+  "img/v8.jpg",
 ];
 var i = 0;
 function cambiarImagen() {
@@ -16,28 +21,3 @@ function cambiarImagen() {
 window.onload = cambiarImagen;
 
 /*******************/
-/*
-var imlocation = "images/";
-function ImageArray(n) {
-  this.length = n;
-  for (var i = 1; i <= n; i++) {
-    this[i] = " ";
-  }
-}
-
-image = new ImageArray(7);
-image[0] = "søndag.jpg";
-image[1] = "mandag.jpg";
-image[2] = "tirsdag.jpg";
-image[3] = "onsdag.jpg";
-image[4] = "torsdag.jpg";
-image[5] = "fredag.jpg";
-image[6] = "lørdag.jpg";
-var currentdate = new Date();
-var imagenumber = currentdate.getDay();
-document.write('<img src="' + imlocation + image[imagenumber] + '"class="center">');
-
-{
-  document.getElementById("dagensevent");
-}
-*/
--- javascript/scriptjeppe.js
+++ javascript/scriptjeppe.js
@@ -6,23 +6,21 @@ var dagligeEvents = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørda
 document.getElementById("title").innerHTML = dagligeEvents[n-1]; 
 */
 
-
 var imlocation = "images/";
- function ImageArray (n) {
-   this.length = n;
-   for (var i =1; i <= n; i++) {
-     this[i] = ' '
-   }
- }
+function ImageArray(n) {
+  this.length = n;
+  for (var i = 1; i <= n; i++) {
+    this[i] = " ";
+  }
+}
 image = new ImageArray(7);
-image[0] = 'sunday.gif';
-image[1] = 'monday.gif';
-image[2] = 'tuesday.gif';
-image[3] = 'wednesday.gif';
-image[4] = 'tirsdag.png';
-image[5] = 'friday.gif';
-image[6] = 'saturday.gif';
+image[0] = "sunday.gif";
+image[1] = "monday.gif";
+image[2] = "tuesday.gif";
+image[3] = "wednesday.gif";
+image[4] = "thursday.gif";
+image[5] = "friday.gif";
+image[6] = "saturday.gif";
 var currentdate = new Date();
 var imagenumber = currentdate.getDay();
 document.write('<img src="' + imlocation + image[imagenumber] + '">');
- 
